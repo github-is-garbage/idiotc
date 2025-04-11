21bb5bc555c99722a02c2425fc078fe4cee5a511
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-04-10 23:32:24 -0400

    Window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,60 @@
-#include <stdio.h>
+#include <windows.h>
+
+const wchar_t CLASS_NAME[] = L"IdiotC";
+
+LRESULT CALLBACK WndProc(HWND hWindow, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+
+		default:
+			return DefWindowProcW(hWindow, uMsg, wParam, lParam);
+	}
+}
 
 int main()
 {
-	printf("HEllO!\n");
-	return 0;
+	HINSTANCE hInstance = GetModuleHandleW(NULL);
+
+	WNDCLASSW wc = { 0 };
+	wc.lpfnWndProc   = WndProc;
+	wc.hInstance     = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+	wc.hCursor       = LoadCursorW(NULL, MAKEINTRESOURCEW(IDC_ARROW));
+	wc.hbrBackground = GetSysColorBrush(COLOR_WINDOW);
+
+	if (!RegisterClassW(&wc))
+		return -1;
+
+	HWND hWindow = CreateWindowExW(
+		0,
+		CLASS_NAME,
+		L"idiotc",
+		WS_OVERLAPPEDWINDOW,
+
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (!hWindow)
+		return -1;
+
+	ShowWindow(hWindow, SW_SHOWDEFAULT);
+	UpdateWindow(hWindow);
+
+	MSG msg;
+	while (GetMessageW(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessageW(&msg);
+	}
+
+	return (int)msg.wParam;
 }
