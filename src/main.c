569c9c025810383f5f5933811243669c23d02ba5
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-04-10 23:55:56 -0400

    Make use of the beautiful resources

--- src/main.c
+++ src/main.c
@@ -1,37 +1,106 @@
 #include <windows.h>
+#include <stdio.h>
 
-const wchar_t CLASS_NAME[] = L"IdiotC";
+#include "resource.h"
 
-LRESULT CALLBACK WndProc(HWND hWindow, UINT uMsg, WPARAM wParam, LPARAM lParam)
+const wchar_t ClassName[] = L"IdiotC";
+
+HBITMAP BlackBitmap;
+HBITMAP WhiteBitmap;
+BOOL IsWhite = TRUE;
+
+LRESULT CALLBACK WindowProcedure(HWND WindowHandle, UINT Message, WPARAM WParam, LPARAM LParam)
 {
-	switch (uMsg)
+	switch (Message)
 	{
+		case WM_SIZE:
+			InvalidateRect(WindowHandle, NULL, TRUE);
+			return 0;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT PaintStruct;
+
+			HDC WindowDC = BeginPaint(WindowHandle, &PaintStruct);
+			HDC MemoryDC = CreateCompatibleDC(WindowDC);
+
+			if (MemoryDC)
+			{
+				HBITMAP CurrentBitmap = IsWhite ? WhiteBitmap : BlackBitmap;
+				HGDIOBJ OldBitmap = SelectObject(MemoryDC, CurrentBitmap);
+
+				BITMAP BitmapInfo;
+				GetObject(CurrentBitmap, sizeof(BITMAP), &BitmapInfo);
+
+				RECT ClientRect;
+				GetClientRect(WindowHandle, &ClientRect);
+
+				int DestinationWidth = ClientRect.right - ClientRect.left;
+				int DestinationHeight = ClientRect.bottom - ClientRect.top;
+
+				SetStretchBltMode(WindowDC, HALFTONE);
+
+				StretchBlt(
+					WindowDC,
+					0, 0, DestinationWidth, DestinationHeight,
+
+					MemoryDC,
+					0, 0, BitmapInfo.bmWidth, BitmapInfo.bmHeight,
+
+					SRCCOPY
+				);
+
+				SelectObject(MemoryDC, OldBitmap);
+				DeleteDC(MemoryDC);
+			}
+
+			EndPaint(WindowHandle, &PaintStruct);
+			return 0;
+		}
+
+		case WM_TIMER:
+			if (WParam == IDT_BITMAP_TOGGLE)
+			{
+				IsWhite = !IsWhite;
+				InvalidateRect(WindowHandle, NULL, TRUE);
+			}
+
+			return 0;
+
 		case WM_DESTROY:
+			KillTimer(WindowHandle, IDT_BITMAP_TOGGLE);
 			PostQuitMessage(0);
+
 			return 0;
 
 		default:
-			return DefWindowProcW(hWindow, uMsg, wParam, lParam);
+			return DefWindowProcW(WindowHandle, Message, WParam, LParam);
 	}
 }
 
 int main()
 {
-	HINSTANCE hInstance = GetModuleHandleW(NULL);
+	HINSTANCE AppInstance = GetModuleHandleW(NULL);
 
-	WNDCLASSW wc = { 0 };
-	wc.lpfnWndProc   = WndProc;
-	wc.hInstance     = hInstance;
-	wc.lpszClassName = CLASS_NAME;
-	wc.hCursor       = LoadCursorW(NULL, MAKEINTRESOURCEW(IDC_ARROW));
-	wc.hbrBackground = GetSysColorBrush(COLOR_WINDOW);
+	BlackBitmap = LoadBitmapW(AppInstance, MAKEINTRESOURCEW(IDB_BLACK));
+	WhiteBitmap = LoadBitmapW(AppInstance, MAKEINTRESOURCEW(IDB_WHITE));
 
-	if (!RegisterClassW(&wc))
+	if (!BlackBitmap || !WhiteBitmap)
 		return -1;
 
-	HWND hWindow = CreateWindowExW(
+	WNDCLASSW WindowClass = { 0 };
+	WindowClass.lpfnWndProc   = WindowProcedure;
+	WindowClass.hInstance     = AppInstance;
+	WindowClass.lpszClassName = ClassName;
+	WindowClass.hCursor       = LoadCursorW(NULL, MAKEINTRESOURCEW(IDC_ARROW));
+	WindowClass.hbrBackground = GetSysColorBrush(COLOR_WINDOW);
+
+	if (!RegisterClassW(&WindowClass))
+		return -1;
+
+	HWND MainWindow = CreateWindowExW(
 		0,
-		CLASS_NAME,
+		ClassName,
 		L"idiotc",
 		WS_OVERLAPPEDWINDOW,
 
@@ -39,22 +108,26 @@ int main()
 
 		NULL,
 		NULL,
-		hInstance,
+		AppInstance,
 		NULL
 	);
 
-	if (!hWindow)
+	if (!MainWindow)
 		return -1;
 
-	ShowWindow(hWindow, SW_SHOWDEFAULT);
-	UpdateWindow(hWindow);
+	ShowWindow(MainWindow, SW_SHOWDEFAULT);
+	UpdateWindow(MainWindow);
+	SetTimer(MainWindow, IDT_BITMAP_TOGGLE, 333, NULL);
 
-	MSG msg;
-	while (GetMessageW(&msg, NULL, 0, 0) > 0)
+	MSG Message;
+	while (GetMessageW(&Message, NULL, 0, 0) > 0)
 	{
-		TranslateMessage(&msg);
-		DispatchMessageW(&msg);
+		TranslateMessage(&Message);
+		DispatchMessageW(&Message);
 	}
 
-	return (int)msg.wParam;
+	DeleteObject(BlackBitmap);
+	DeleteObject(WhiteBitmap);
+
+	return (int)Message.wParam;
 }
